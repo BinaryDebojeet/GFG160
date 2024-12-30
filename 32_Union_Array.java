0cfbe33ac58ba1443198fcd7823fc4364ef6232c
Debojeet Dutta <debojeetdutta43@gmail.com>
2024-12-30 22:57:25 +0530

    day 32

---
+++ 32_Union_Array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution{
+    public static int findUnion(int[] a, int[] b){
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num: a){
+            set.add(num);
+        }
+        for(int num: b){
+            set.add(num);
+        }
+
+        return set.size();
+    }
+}
