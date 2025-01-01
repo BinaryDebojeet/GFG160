5758d6de70a2c5c8e89fe31fd5838ea2231bc7b1
Debojeet Dutta <debojeetdutta43@gmail.com>
2025-01-01 20:01:37 +0530

    day 34

---
+++ 34_Print_Anagrams.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public ArrayList<ArrayList<String>> anagrams(String[] arr) {
+        
+        ArrayList<ArrayList<String>> ans = new ArrayList<>();
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        
+        for(String s : arr){
+            
+            char[] temp = s.toCharArray();
+            Arrays.sort(temp);
+            String str = new String(temp);
+            
+            map.putIfAbsent(str, new ArrayList<>());
+            map.get(str).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
