bacf6e6c024a9e31e117bb1d587ce4cf8b427dc4
Debojeet Dutta <debojeetdutta43@gmail.com>
2025-01-08 23:07:32 +0530

    day 41

---
+++ 41_Number_Of_Triangles.java
@@ -0,0 +1,27 @@
+class Solution {
+    // Function to count the number of possible triangles.
+    static int countTriangles(int arr[]) {
+        
+        int n = arr.length;
+        int ans = 0;
+        Arrays.sort(arr);
+        for(int i=n-1; i>=2; i--){ 
+            
+            int left = 0;
+            int right = i-1;
+            
+            while(left < right){
+                
+                int sum = arr[left] + arr[right];
+                if(sum > arr[i]){
+                    ans+= right - left;
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return ans;
+    }
+}
