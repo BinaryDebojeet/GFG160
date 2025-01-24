3cac526ccc63a024de3d349f732b40c6990168ad
Debojeet Dutta <debojeetdutta43@gmail.com>
2025-01-24 21:33:26 +0530

    day 57

---
+++ 57_Detect_Loop_InList.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static boolean detectLoop(Node head) {
+        
+        Node slow = head;
+        Node fast = head;
+        
+        if(head == null || head.next == null ) return false;
+        
+        while(fast!=null){
+           
+           if(fast.next==null){
+               return false;
+           }
+           fast = fast.next.next;
+           if(slow==null){
+               return false;
+           }
+           slow = slow.next;
+           if(slow == fast){
+               return true;
+           }
+        }
+        
+        return false;
+    }
+}
