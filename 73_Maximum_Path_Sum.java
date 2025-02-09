1497a8cc34df547da7087f7db940fedb369216d7
Debojeet Dutta <debojeetdutta43@gmail.com>
2025-02-09 23:36:25 +0530

    added

---
+++ 73_Maximum_Path_Sum.java
@@ -0,0 +1,16 @@
+// class Solution {
+//     int max=Integer.MIN_VALUE;
+//     int path(Node root)
+//     {
+//         if(root==null) return 0;
+//         int l=Math.max(0,path(root.left));
+//         int r=Math.max(0,path(root.right));
+//         max=Math.max(max,root.data+l+r);
+//         return root.data+Math.max(l,r);
+//     }
+//     int findMaxSum(Node node) {
+        
+//         path(node);
+//         return max;
+//     }
+// }
